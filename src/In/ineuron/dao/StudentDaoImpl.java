6c91710c89dba7a95a16f5f93b41f565427bd09c
Ankit17028 <ankitbansod2000@gmail.com>
2023-06-04 12:32:19 +0530

    first commit

---
+++ src/In/ineuron/dao/StudentDaoImpl.java
@@ -0,0 +1,78 @@
+package In.ineuron.dao;
+
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import In.ineuron.model.Student;
+import In.ineuron.util.HibernateUtil;
+
+public class StudentDaoImpl implements IStudentDao {
+
+	Session session = HibernateUtil.getSession();
+	
+	
+	@Override
+	public String save(Student student) {
+		Transaction	transaction	= session.beginTransaction();
+		session.save(student);
+		boolean flag = true;
+		String status = null;
+		if (flag) {
+			transaction.commit();
+			status = "success";
+		} else {
+			transaction.rollback();
+			status = "failure";
+		}
+		return status;
+	}
+
+	@Override
+	public Student findById(Integer sid) {
+		Student student = session.get(Student.class, sid);
+		return student;
+		
+		
+	}
+
+	@Override
+	public String updateById(Student student) {
+		Transaction	transaction	= session.beginTransaction();
+		session.saveOrUpdate(student);
+		boolean flag = true;
+		String status = null;
+		if (flag) {
+			transaction.commit();
+			status = "success";
+		} else {
+			transaction.rollback();
+			status = "failure";
+		}
+		return status;
+	}
+
+	@Override
+	public String deleteById(Integer sid) {
+		Transaction	transaction	= session.beginTransaction();
+		Student student = findById(sid);
+		if (student != null) {
+			session.delete(student);
+			boolean flag = true;
+			String status = null;
+			if (flag) {
+				transaction.commit();
+				status = "success";
+			} else {
+				transaction.rollback();
+				status = "failure";
+			}
+			return status;
+		}
+		else {
+			return "notfound";
+		}
+		
+	}
+
+}
---
+++ src/In/ineuron/util/HibernateUtil.java
@@ -0,0 +1,49 @@
+package In.ineuron.util;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+	static Configuration cfg = null;
+	static SessionFactory sessionFactory = null;
+	static Session session = null;
+	static {
+		try {
+			cfg = new Configuration();
+			cfg.configure();
+//			System.out.println(cfg.getProperties());
+		
+			sessionFactory = cfg.buildSessionFactory();
+
+		} catch (HibernateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public static Session getSession(){
+		if (session == null)
+			session = sessionFactory.openSession();
+		return session;		
+	}
+	public static void closeSession(Session session) {
+		if (session != null) {
+			session.close();
+		}
+	}
+	public static void closeSessionFactory(SessionFactory sessionFactory) {
+		if (sessionFactory != null) {
+			sessionFactory.close();
+		}
+	}
+	public static void startUp() {
+		System.out.println("*****StartingUp hibernate environment.....");
+	}
+	
+	
+	
+}
